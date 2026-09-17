1ce2d681f70e2258c9dabc332c1e2070c0704cac
Livan Torres <livantorresnunez@gmail.com>
2026-09-17 10:34:03 -0500

    chore: limpieza de archivos de prueba y scripts temporales

--- download_audio.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = 'https://actions.google.com/sounds/v1/alarms/beep_short.ogg';
-$data = file_get_contents($url);
-file_put_contents('assets/sounds/notification.mp3', $data); // Saving ogg as mp3 extension is fine for HTML5 audio mostly, but let's just use the file as is, the browser will decode it.
-echo "Audio downloaded.";
--- test_stream.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'includes/db.php';
-$_SESSION['user_id'] = 1;
-require_once 'api/stream.php';
